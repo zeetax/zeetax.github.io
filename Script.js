79732353485f08668dd654e42f370526bfea1838
zeetax <zeetaxsolutions183@gmail.com>
2026-02-28 00:43:14 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,7 @@
-// Counter Animation
-document.querySelectorAll('.counter').forEach(counter=>{
-  counter.innerText='0';
-  const update=()=>{
-    const target=+counter.getAttribute('data-target');
-    const c=+counter.innerText;
-    const increment=target/200;
-    if(c<target){
-      counter.innerText=`${Math.ceil(c+increment)}`;
-      setTimeout(update,10);
-    }else{
-      counter.innerText=target;
-    }
-  };
-  update();
+// Smooth scroll for nav links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'});
+    });
 });
