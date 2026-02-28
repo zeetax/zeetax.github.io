5a00029df4c4db51db782eecc0eb9a52b3c26b88
zeetax <zeetaxsolutions183@gmail.com>
2026-02-28 06:08:23 +0500

    Refactor smooth scroll functionality for anchors

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
-// Smooth scroll for nav links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e){
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'});
-    });
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+anchor.addEventListener("click",function(e){
+e.preventDefault();
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({behavior:"smooth"});
+});
 });
